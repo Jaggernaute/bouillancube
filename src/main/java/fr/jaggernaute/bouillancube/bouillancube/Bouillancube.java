0066dbe24578c308bccf1ee424631df77a8f9cdb
lucas <lucas.videcoq1@gmail.com>
2021-03-31 12:12:34 +0200

    Initial commit

---
+++ src/main/java/fr/jaggernaute/bouillancube/bouillancube/Bouillancube.java
@@ -0,0 +1,17 @@
+package fr.jaggernaute.bouillancube.bouillancube;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bouillancube extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
