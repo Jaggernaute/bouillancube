cc8f03fabf495b27295a7f32c91b7589373e6612
lucas <lucas.videcoq1@gmail.com>
2021-04-20 13:16:24 +0200

    Fini : - add circleci config.yml
    Reste:
    -circleci config.yml configuration
    -change classes name to respect the class naming convention
    -stats de chaques mobs (ca va etre long xD)
    -rewards
    -Easter eggs

--- src/main/java/fr/jaggernaute/bouillancube/gui/guistats/GUIStatsBlockMenu.java
+++ src/main/java/fr/jaggernaute/bouillancube/gui/guistats/GUIStatsBlockMenu.java
@@ -1,6 +1,5 @@
 package fr.jaggernaute.bouillancube.gui.guistats;
 
-import fr.jaggernaute.bouillancube.gui.MenuHandler;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -8,7 +7,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
 import java.util.ArrayList;
 
 import static org.bukkit.Statistic.MINE_BLOCK;
