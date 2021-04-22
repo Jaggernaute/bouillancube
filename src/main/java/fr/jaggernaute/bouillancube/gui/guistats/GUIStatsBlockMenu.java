e834d8890bdb9f20b6b90f9f496449108b7efb4a
lucas <lucas.videcoq1@gmail.com>
2021-04-22 10:59:41 +0200

    Fini : -add a char in the .circleci directory (I'm a moron).
    Reste:
    -circleci config.yml configuration
    -change classes name to respect the class naming convention
    -stats de chaques mobs (ca va etre long xD)
    -rewards
    -Easter eggs

--- src/main/java/fr/jaggernaute/bouillancube/gui/guistats/GUIStatsBlockMenu.java
+++ src/main/java/fr/jaggernaute/bouillancube/gui/guistats/GUIStatsBlockMenu.java
@@ -8,6 +8,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import java.util.ArrayList;
+import fr.jaggernaute.bouillancube.gui.MenuHandler;
 
 import static org.bukkit.Statistic.MINE_BLOCK;
 
--- src/main/java/fr/jaggernaute/bouillancube/gui/guistats/GUIStatsHome.java
+++ src/main/java/fr/jaggernaute/bouillancube/gui/guistats/GUIStatsHome.java
@@ -1,13 +1,15 @@
 package fr.jaggernaute.bouillancube.gui.guistats;
 
-import fr.jaggernaute.bouillancube.gui.MenuHandler;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
+
+import fr.jaggernaute.bouillancube.gui.MenuHandler;
 
 public class GUIStatsHome extends MenuHandler {
 
